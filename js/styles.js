e07b5e6d5723f44af9d4f1d43a5e658e40a0e5ba
Guest User <Guest@epicodus-10.local>
2018-01-25 13:11:26 -0800

    adds thml and js elements

--- js/styles.js
+++ js/styles.js
@@ -1,23 +1,42 @@
 
-var randomNumber = Math.floor(Math.random() * 6);
-
-function randomWholeNum() {
-
-  return Math.random();
+function randomNumber () {
+  return (Math.floor(6*Math.random())+1);
 }
+
+
+var player1totalScore = [0,0]; //totalScore[0] = (whatever you player 1 score to be)
+var player1roundScore = 0;
+
+var player2totalScore = [0,0]; //totalScore[0] = (whatever you player 1 score to be)
+var player2roundScore = 0;
+
+
 
 $(document).ready(function() {
   $("button#player1").click(function() {
-    $("ul").prepend(randomNumber);
+    var newRand = randomNumber();
+    player1roundScore += newRand;
+    $("#player1Roll").text(newRand);
+    $("#player1Score").text(player1roundScore);
+
+    // if (newRand === 1)
+    //   $
+    //   alert("")
+
+    if (player1roundScore === 100) {
+      alert("You Win!")
+    }
   });
 
-
-
-
-
   $("button#player2").click(function() {
-    event.preventDefault();
-    $("ul").prepend(randomNumber);
+    var newRand = randomNumber();
+    player2roundScore += newRand;
+    $("#player2Roll").text(newRand);
+    $("#player2Score").text(player2roundScore);
+
+    if (player2roundScore === 100) {
+      alert("You Win!")
+    }
   });
 
 });
